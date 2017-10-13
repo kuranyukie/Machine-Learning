a1de7916edffe057764221493a6a14a5eb00874b
kuranyukie <selinadora@126.com>
2017-10-13 17:41:52 -0400

    finish ex3

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -40,14 +40,7 @@ J = ( (1/m) * sum(-y' * log(sigmoid(X*theta)) - (1-y)' * log( 1 - sigmoid(X*thet
 
 grad = (1/m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
-grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
-
-
-
-
-
-
-
+grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m) * theta(2:length(theta))';
 
 
 % =============================================================
